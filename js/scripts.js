c5dd6766301709f07581e334b1f8c961a7e1d1e3
Sam Gespass <samuel.gespass@gmail.com>
2017-12-19 09:36:07 -0800

    add test furniture objects and commented out constructor for new furniture

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,34 @@ function dice(){
   return Math.floor((Math.random()*10)+1)
 }
 //1 space represents a 5ft * 5ft square
+
+var chair = function(onTop) {
+  this.size: 1;
+  this.onTop: onTop
+}
+
+var table = function(onTop) {
+  this.size: 4;
+  this.onTop: onTop
+}
+
+var bed = function(onTop) {
+  this.size: 6;
+  this.onTop: onTop
+}
+
+var chest = function(inside) {
+  this.size: 2;
+  this.inside: inside
+}
+
+var armoire = function(inside) {
+  this.size: 3;
+  this.inside: inside;
+}
+
+// var furntiure = function(name, size, onTop) {
+//   this.name: name;
+//   this.size: size;
+//   this.onTop: onTop
+// }
