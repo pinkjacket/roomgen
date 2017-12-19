4a9a995fee0738046134cc1027177f566d249773
Sam Gespass <samuel.gespass@gmail.com>
2017-12-19 10:13:45 -0800

    add more furniture objects

--- js/scripts.js
+++ js/scripts.js
@@ -10,27 +10,70 @@ function dice(){
 
 var chair = function(onTop) {
   this.size: 1;
-  this.onTop: onTop
+  this.onTop: onTop;
+}
+
+var throne = function(onTop) {
+  this.size: 2;
+  this.onTop: onTop;
 }
 
 var table = function(onTop) {
   this.size: 4;
-  this.onTop: onTop
+  this.onTop: onTop;
 }
 
 var bed = function(onTop) {
   this.size: 6;
-  this.onTop: onTop
+  this.onTop: onTop;
 }
 
 var chest = function(inside) {
   this.size: 2;
-  this.inside: inside
+  this.inside: inside;
 }
 
 var armoire = function(inside) {
   this.size: 3;
   this.inside: inside;
+}
+
+var couch = function(onTop) {
+  this.size: 4;
+  this.onTop: onTop;
+}
+
+var nightStand = function(onTop, inside) {
+  this.size: 1;
+  this.onTop: onTop;
+  this.inside: inside;
+}
+
+var chestOfDrawers = function(onTop, inside) {
+  this.size: 4;
+  this.onTop: onTop;
+  this.inside: inside;
+}
+
+var counter = function(onTop, inside) {
+  this.size: 1;
+  this.onTop: onTop;
+  this.inside: inside;
+}
+
+var desk = function(onTop, inside) {
+  this.size: 3;
+  this.onTop: onTop;
+  this.inside = inside;
+}
+
+var ottoman = function() {
+  this.size: 1;
+}
+
+var bench = function(onTop) {
+  this.size: 4;
+  this.onTop: onTop;
 }
 
 // var furntiure = function(name, size, onTop) {
