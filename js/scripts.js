3ec60b3b9380fc1caf765c27bd6978026ef307c0
Adam Calhoun and Ryan Ferris <ryan@>
2017-12-20 15:22:12 -0800

    update logic to only display furniture items when array is not empty.

--- js/scripts.js
+++ js/scripts.js
@@ -87,38 +87,62 @@ var itemArray = [];
 $(document).ready(function(){
 
   function furnitureInside(furnitureItemsInside, idInside) {
-    // var idInside = 0
     furnitureItemsInside.forEach(function(itemInside) {
-      // idInside = idInside + 1;
       $("ul#" + idInside).append("<li>" + itemInside.name  + "</li></ul>")
     });
   };
 
   function furnitureOnTop(furnitureItemsOnTop, idOnTop) {
-    // var idOnTop = 0
     furnitureItemsOnTop.forEach(function(itemOnTop) {
-      // idOnTop = idOnTop + 1;
       $("ul.item-on-top#"+ idOnTop).append("<li>" + itemOnTop.name  + "</li></ul>")
     });
   };
 
+  function insideHeader(furnitureItemsInside, itemName, idInside) {
+    console.log(furnitureItemsInside);
+
+    //console.log("items inside: " + furnitureItemsInside.length);
+    if (furnitureItemsInside.length === 0) {
+      // $("ul.furniture").append("<h3>inside the the " + furnitureItemsInside.name + " is: </h3><ul class='item-inside' id='" + idInside + "'></ul>");
+      console.log("Inside items array is empty.");
+      return ''
+    } else {
+      return ("<h3>inside the " + itemName + " is: </h3><ul class='item-inside' id='" + idInside + "'></ul>");
+    }
+  }
+
+  function onTopHeader(furnitureOnTop, itemName, idOnTop) {
+    console.log("idInside header: "+ idOnTop);
+    //console.log("items on top: " + furnitureOnTop.length);
+    if (furnitureOnTop.length === 0) {
+      // $("ul.furniture").append("<h3>and on top of the " + furnitureOnTop.name + " is: </h3><ul class='item-on-top' id='" + idOnTop + "'></ul>");
+      console.log("On-top items array is empty.");
+      return ''
+    } else {
+      return ("<h3>and on top of the " + itemName + " is: </h3><ul class='item-on-top' id='" + idOnTop + "'></ul>")
+    }
+  }
+
   function roomFurniture(roomFurnitureArray) {
+    // debugger;
     var idNum = 0;
     var idInside = "idInside" + idNum;
     var idOnTop = "idOnTop"+idNum
-    console.log("idFurniture: " + idInside);
+    //console.log("inside ID: " + idInside);
     roomFurnitureArray.forEach(function(furnishedItem) {
       idNum = idNum + 1;
       idInside = "idInside" + idNum;
       idOnTop = "idOnTop" + idNum;
-      $("ul.furniture").append("<li>" + furnishedItem.name  + "</li>" + "<h3>inside the the " + furnishedItem.name + " is: </h3><ul class='item-inside' id='" + idInside + "'></ul>" + "<h3>and on top of the " + furnishedItem.name + " is:</h3><ul class='item-on-top' id='" + idOnTop + "'></ul>")
+      $("ul.furniture").append("<li><h2>" + furnishedItem.name  + "</h2></li>" +
+
+      insideHeader(furnishedItem.insideArray, furnishedItem.name, idInside) +
+      onTopHeader(furnishedItem.onTopArray, furnishedItem.name, idOnTop));
+
       furnitureInside(furnishedItem.insideArray, idInside);
+      console.log("idInside: "+ idInside);
       furnitureOnTop(furnishedItem.onTopArray, idOnTop);
     })
   };
-
-
-
 
   $("form#room").submit(function() {
     event.preventDefault();
